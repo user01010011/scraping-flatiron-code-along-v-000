1d487dc8e7b6389486f3e2a584bb68e3d12df276
Shasha Yan <shashayan@gmail.com>
2021-05-19 22:33:43 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -8,7 +8,9 @@ class Scraper
 
   def get_page
     doc = Nokogiri::HTML(open("http://learn-co-curriculum.github.io/site-for-scraping/courses"))
-
     binding.pry
   end
+  
 end
+
+Scraper.new.get_page
